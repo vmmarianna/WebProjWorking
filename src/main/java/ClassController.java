4c7f061407fb716f4896e749586b31e36689d709
vmmarianna <marina9021089@gmail.com>
2019-08-26 20:53:16 +0300

    ClassController

---
+++ src/main/java/ClassController.java
@@ -0,0 +1,37 @@
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+@RequestMapping("/class")
+public class ClassController {
+
+    private List<ClassController> classList = new ArrayList<>();
+    private int id = 1;
+
+    @RequestMapping(value = "", method = RequestMethod.GET)
+    public ModelAndView result() {
+        ModelAndView modelView = new ModelAndView("/test-result");
+        modelView.addObject("classList", classList);
+        return modelView;
+    }
+
+    @RequestMapping(value = "/test-form", method = RequestMethod.GET)
+    public ModelAndView showForm() {
+        return new ModelAndView("test-form", "class", new ClassController());
+    }
+
+    @RequestMapping(value = "/postForm", method = RequestMethod.POST)
+    public String submitForm(@ModelAttribute("classController") ClassController classController, ModelMap model) {
+        classList.add(new ClassController(id++, classController.getName(), classController.getDescription()));
+        return "redirect:/class";
+    }
+
+}
